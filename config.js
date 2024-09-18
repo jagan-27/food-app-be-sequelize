87e70b3e051ae925f3e084760eba0658fe407a4e
Subash Manian <maniansubash@gmail.com>
2024-09-18 19:48:52 +0530

    db config updated

--- config.js
+++ config.js
@@ -7,9 +7,9 @@ const options = { directory: output, caseFile: 'c', caseModel: 'c', caseProp: 'c
  };
 
 const postgres = {
-    dbname: 'examplecreatepg',         // Extracted from the connection string
-    user: 'examplecreatepg_user',      // Extracted from the connection string
-    pass: 'HUxzz2R4Cxr7qaVtZMO2nxOdkDe92L9l',  // Password extracted from the connection string
+    dbname: 'foodiesmap_prod',         // Extracted from the connection string
+    user: 'foodiesmap_prod_user',      // Extracted from the connection string
+    pass: 'iPCPgz9PTMuBpN8nsxGZTCp5Ja8KrPzX',  // Password extracted from the connection string
     autoOptions: {
         dialect: 'postgres',
         dialectOptions: {
@@ -17,7 +17,7 @@ const postgres = {
                 require: true, // This will ensure SSL is used
             }
         },
-        host: 'dpg-cr4mp788fa8c73a4b6l0-a.singapore-postgres.render.com',
+        host: 'dpg-crldccrtq21c73ed2gs0-a.singapore-postgres.render.com',
         port: 5432,
         ...options
     }  
